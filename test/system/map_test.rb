6ef04b77e9f82e29371bccda7cd0f1718b7ab1dc
vitaotm <vitor.grosskopf@gmail.com>
2026-06-23 12:24:03 -0300

    fix event click for svg on system test

--- test/system/map_test.rb
+++ test/system/map_test.rb
@@ -10,28 +10,32 @@ class MapTest < ApplicationSystemTestCase
     visit map_url
     assert_text "My Reading Map"
 
-    # Verify SVG map is loaded
+    # Wait until Stimulus has connected and bound click listeners
     assert_selector "[data-world-map-connected='true']"
+
+    # Verify SVG map is loaded
     assert_selector "svg"
-    #
-    # # Click on a country (e.g. Brazil - BR)
-    find("path#BR.land").click
-    #
-    # # Assert map panel color configuration card appears
+
+    # Click on Brazil (using JS dispatchEvent to click safely on SVG element in headless CI)
+    find("path#BR.land").execute_script("this.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable:
+true }));")
+
+    # Assert map panel color configuration card appears
     assert_selector "#map-panel"
     assert_selector "#map-panel-title", text: "BR"
-    #
-    # # Save color
+
+    # Save color
     click_on "Save Color"
-    #
-    # # Verify country code appears under 'Marked countries' (Capybara waits for JS reload)
+
+    # Verify country code appears under 'Marked countries' (waits for JS reload)
     assert_text "BR"
-    #
-    # # Clear country
-    find("path#BR").click
+
+    # Click Brazil again to clear (using JS dispatchEvent)
+    find("path#BR").execute_script("this.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true
+}));")
     click_on "Clear"
-    #
-    # # Verify country code is removed from list (Capybara waits for JS reload)
+
+    # Verify country code is removed from list (waits for JS reload)
     refute_text "BR"
   end
 end
